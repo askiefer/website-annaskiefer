25acd5a6051344c9be79cb9804415478f519f8b2
Anna Kiefer <annaskiefer@gmail.com>
2016-06-23 10:12:38 -0700

    My site ready for deployment

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/akiefer/Desktop/hackbright/personal-website/startbootstrap-agency/env"
+set -gx VIRTUAL_ENV "/Users/akiefer/Desktop/hackbright/personal-website/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
